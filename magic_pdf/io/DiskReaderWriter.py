d876cbe87ba6a49dba1b14052acf0044aed8e579
liukaiwen <liukaiwen@pjlab.org.cn>
2024-04-15 19:22:05 +0800

    io modules

--- magic_pdf/io/DiskReaderWriter.py
+++ magic_pdf/io/DiskReaderWriter.py
@@ -33,6 +33,9 @@ class DiskReaderWriter(AbsReaderWriter):
             abspath = path
         else:
             abspath = os.path.join(self.path, path)
+        if not os.path.exists(abspath):
+            directory_path = os.path.dirname(abspath)
+            os.makedirs(directory_path)
         if mode == MODE_TXT:
             with open(abspath, 'w', encoding=self.encoding) as f:
                 f.write(content)
@@ -50,11 +53,11 @@ class DiskReaderWriter(AbsReaderWriter):
 
 # 使用示例
 if __name__ == "__main__":
-    file_path = "io/example.txt"
+    file_path = "io/test/example.txt"
     drw = DiskReaderWriter("D:\projects\papayfork\Magic-PDF\magic_pdf")
 
     # 写入内容到文件
-    drw.write(b"Hello, World!", path="io/example.txt", mode="binary")
+    drw.write(b"Hello, World!", path="io/test/example.txt", mode="binary")
 
     # 从文件读取内容
     content = drw.read(path=file_path)
