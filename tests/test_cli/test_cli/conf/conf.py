7d59d329f6ffdde9d09df92c7f0c099e005dda94
quyuan <quyuan@pjlab.org>
2024-04-25 10:54:19 +0800

    add cli test

--- tests/test_cli/test_cli/conf/conf.py
+++
@@ -1,8 +0,0 @@
-import os
-conf = {
-"code_path": os.environ.get('CI_PROJECT_DIR'),
-"pdf_dev_path" : os.environ.get('CI_PROJECT_DIR') + "/tests/pdf_dev",
-"pdf_res_path": "/home/quyuan/code/Magic-PDF/Magic-PDF/Magic-PDF/data"
-
-}
-
