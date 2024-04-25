f3ad1caa8497437cbcb01a2da08732c12a54c437
quyuan <quyuan@pjlab.org>
2024-04-25 13:05:58 +0800

    add cli test

--- tests/test_cli/conf/conf.py
+++ tests/test_cli/conf/conf.py
@@ -1,7 +1,7 @@
 import os
 conf = {
 "code_path": os.environ.get('GITHUB_WORKSPACE'),
-"pdf_dev_path" : os.environ.get('GITHUB_WORKSPACE') + "/tests/test_cli/pdf_dev",
+"pdf_dev_path" : os.environ.get('GITHUB_WORKSPACE') + "/tests/test_cli/pdf-dev",
 "pdf_res_path": "/home/quyuan/code/Magic-PDF/Magic-PDF/Magic-PDF/data"
 
 }
