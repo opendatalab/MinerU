e980d2efa0843e9d50ba834e51ebc6c07a3bad04
赵小蒙 <moe@myhloli.com>
2024-04-29 14:47:30 +0800

    fix UNIPipe and spans space with language

--- magic_pdf/dict2md/ocr_mkcontent.py
+++ magic_pdf/dict2md/ocr_mkcontent.py
@@ -159,10 +159,10 @@ def merge_para_with_text(para_block):
                 content = f"\n$$\n{span['content']}\n$$\n"
 
             if content != '':
-                if language == 'en':  # 英文语境下 content间需要空格分隔
-                    para_text += content + ' '
-                else:  # 中文语境下，content间不需要空格分隔
-                    para_text += content
+                if 'zh' in language:
+                    para_text += content  # 中文语境下，content间不需要空格分隔
+                else:
+                    para_text += content + ' '  # 英文语境下 content间需要空格分隔
     return para_text
 
 
