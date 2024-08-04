252139099b3f3ccab9045ebad9aa4ba5182b86c4
myhloli <moe@myhloli.com>
2024-08-04 20:29:39 +0800

    fix(setup): allow latest matplotlib versions on non-Windows platforms
    The restriction on the matplotlib version has been updated to only apply on Windows
    platforms, where precompiled packages are not available starting from version 3.9.1.
    This change enables users on Linux and macOS to install newer versions of matplotlib,
    addressing compatibility issues with recent bug fixes.

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
                      "paddlepaddle==2.6.1;platform_system=='Windows' or platform_system=='Darwin'",
                      ],
             "full": ["unimernet==0.1.6",  # 0.1.6版本大幅裁剪依赖包范围，推荐使用此版本
-                     "matplotlib<=3.9.0",  # 3.9.1及之后不提供windows的预编译包，避免一些没有编译环境的windows设备安装失败
+                     "matplotlib<=3.9.0;platform_system=='Windows'",  # 3.9.1及之后不提供windows的预编译包，避免一些没有编译环境的windows设备安装失败
+                     "matplotlib;platform_system=='Linux' or platform_system=='Darwin'",  # linux 和 macos 不应限制matplotlib的最高版本，以避免无法更新导致的一些bug
                      "ultralytics",  # yolov8,公式检测
                      "paddleocr==2.7.3",  # 2.8.0及2.8.1版本与detectron2有冲突，需锁定2.7.3
                      "paddlepaddle==3.0.0b1;platform_system=='Linux'",  # 解决linux的段异常问题
