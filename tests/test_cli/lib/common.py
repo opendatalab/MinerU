2a83440f348d5a2e8015c2bbb05727651dba54d0
quyuan <quyuan@pjlab.org>
2024-08-13 17:20:16 +0800

    add ci case

--- tests/test_cli/lib/common.py
+++ tests/test_cli/lib/common.py
@@ -13,10 +13,17 @@ def count_folders_and_check_contents(file_path):
     """"
     获取文件夹大小
     """
+    check_path = os.path.join(file_path, "auto")
     if os.path.exists(file_path):
-        folder_count = os.path.getsize(file_path)
-        assert folder_count > 0
+        folder_count = os.path.getsize(check_path)
+        assert folder_count > 5
 
+def delete_file(file_path):
+    """
+    删除文件
+    """
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
 if __name__ == "__main__":
     count_folders_and_check_contents("/home/quyuan/code/Magic-PDF/Magic-PDF/Magic-PDF/ci") 
