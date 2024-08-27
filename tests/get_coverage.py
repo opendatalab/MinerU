dce77e72520b94f131337e9f83bc2067663a4276
quyuan <quyuan@pjlab.org>
2024-08-27 19:27:16 +0800

    feat: add test case

--- tests/get_coverage.py
+++ tests/get_coverage.py
@@ -16,4 +16,7 @@ def get_covrage():
     percentage_value = pc_cov_span.text.strip()
     percentage_float = float(percentage_value.rstrip('%'))
     print ("percentage_float:", percentage_float)
-    assert percentage_float > 0.2
+    assert percentage_float >= 0.2
+
+if __name__ == '__main__':
+    get_covrage()
