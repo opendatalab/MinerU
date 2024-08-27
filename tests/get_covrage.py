20d0b879946e3d78f13f4f303a6ae62015135781
quyuan <quyuan@pjlab.org>
2024-08-27 14:51:10 +0800

    feat: add test case

---
+++ tests/get_covrage.py
@@ -0,0 +1,18 @@
+"""
+get cov
+"""
+import BeautifulSoup
+
+def get_covrage():
+    """get covrage"""
+    # 发送请求获取网页内容
+    html_content = open("html_cov/index.html", "r", encoding="utf-8").read()
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # 查找包含"pc_cov"的span标签
+    pc_cov_span = soup.find('span', class_='pc_cov')
+
+    # 提取百分比值
+    percentage_value = pc_cov_span.text.strip()
+    percentage_float = float(percentage_value.rstrip('%'))
+    assert percentage_float > 0.2
