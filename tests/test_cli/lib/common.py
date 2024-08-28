3d2fb83665daa51cf894a417517005d8cc183eb7
yyy <102640628+dt-yy@users.noreply.github.com>
2024-08-28 14:34:21 +0800

    feat: add test case (#499)
    Co-authored-by: quyuan <quyuan@pjlab.org>

--- tests/test_cli/lib/common.py
+++ tests/test_cli/lib/common.py
@@ -1,22 +1,43 @@
-import subprocess
+"""common definitions."""
 import os
+import shutil
 
 
 def check_shell(cmd):
-    """
-    shell successful
-    """
+    """shell successful."""
     res = os.system(cmd)
     assert res == 0
 
-def count_folders_and_check_contents(file_path):
-    """"
-    获取文件夹大小
-    """
+
+def cli_count_folders_and_check_contents(file_path):
+    """" count cli files."""
     if os.path.exists(file_path):
-        folder_count = os.path.getsize(file_path)
-        assert folder_count > 0
+        for files in os.listdir(file_path):
+            folder_count = os.path.getsize(os.path.join(file_path, files))
+            assert folder_count > 0
+    assert len(os.listdir(file_path)) > 5
+
+def sdk_count_folders_and_check_contents(file_path):
+    """count folders."""
+    if os.path.exists(file_path):
+        file_count = os.path.getsize(file_path)
+        assert file_count > 0
+    else:
+        exit(1)
 
 
-if __name__ == "__main__":
-    count_folders_and_check_contents("/home/quyuan/code/Magic-PDF/Magic-PDF/Magic-PDF/ci") 
+def delete_file(path):
+    """delete file."""
+    if not os.path.exists(path):
+        if os.path.isfile(path):
+            try:
+                os.remove(path)
+                print(f"File '{path}' deleted.")
+            except TypeError as e:
+                print(f"Error deleting file '{path}': {e}")
+    elif os.path.isdir(path):
+        try:
+            shutil.rmtree(path)
+            print(f"Directory '{path}' and its contents deleted.")
+        except TypeError as e:
+            print(f"Error deleting directory '{path}': {e}")
--- tests/test_tools/test_cli_dev.py
+++ tests/test_tools/test_cli_dev.py
@@ -1,7 +1,6 @@
+import tempfile
 import os
 import shutil
-import tempfile
-
 from click.testing import CliRunner
 
 from magic_pdf.tools import cli_dev
@@ -9,23 +8,22 @@ from magic_pdf.tools import cli_dev
 
 def test_cli_pdf():
     # setup
-    unitest_dir = '/tmp/magic_pdf/unittest/tools'
-    filename = 'cli_test_01'
+    unitest_dir = "/tmp/magic_pdf/unittest/tools"
+    filename = "cli_test_01"
     os.makedirs(unitest_dir, exist_ok=True)
-    temp_output_dir = tempfile.mkdtemp(dir='/tmp/magic_pdf/unittest/tools')
-    os.makedirs(temp_output_dir, exist_ok=True)
+    temp_output_dir = tempfile.mkdtemp(dir="/tmp/magic_pdf/unittest/tools")
 
     # run
     runner = CliRunner()
     result = runner.invoke(
         cli_dev.cli,
         [
-            'pdf',
-            '-p',
-            'tests/test_tools/assets/cli/pdf/cli_test_01.pdf',
-            '-j',
-            'tests/test_tools/assets/cli_dev/cli_test_01.model.json',
-            '-o',
+            "pdf",
+            "-p",
+            "tests/test_tools/assets/cli/pdf/cli_test_01.pdf",
+            "-j",
+            "tests/test_tools/assets/cli_dev/cli_test_01.model.json",
+            "-o",
             temp_output_dir,
         ],
     )
@@ -33,31 +31,31 @@ def test_cli_pdf():
     # check
     assert result.exit_code == 0
 
-    base_output_dir = os.path.join(temp_output_dir, 'cli_test_01/auto')
+    base_output_dir = os.path.join(temp_output_dir, "cli_test_01/auto")
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_content_list.json'))
+    r = os.stat(os.path.join(base_output_dir, "content_list.json"))
     assert r.st_size > 5000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}.md'))
+    r = os.stat(os.path.join(base_output_dir, f"{filename}.md"))
     assert r.st_size > 7000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_middle.json'))
+    r = os.stat(os.path.join(base_output_dir, "middle.json"))
     assert r.st_size > 200000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_model.json'))
+    r = os.stat(os.path.join(base_output_dir, "model.json"))
     assert r.st_size > 15000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_origin.pdf'))
+    r = os.stat(os.path.join(base_output_dir, "origin.pdf"))
     assert r.st_size > 500000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_layout.pdf'))
+    r = os.stat(os.path.join(base_output_dir, "layout.pdf"))
     assert r.st_size > 500000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_spans.pdf'))
+    r = os.stat(os.path.join(base_output_dir, "spans.pdf"))
     assert r.st_size > 500000
 
-    assert os.path.exists(os.path.join(base_output_dir, 'images')) is True
-    assert os.path.isdir(os.path.join(base_output_dir, 'images')) is True
+    assert os.path.exists(os.path.join(base_output_dir, "images")) is True
+    assert os.path.isdir(os.path.join(base_output_dir, "images")) is True
 
     # teardown
     shutil.rmtree(temp_output_dir)
@@ -65,27 +63,26 @@ def test_cli_pdf():
 
 def test_cli_jsonl():
     # setup
-    unitest_dir = '/tmp/magic_pdf/unittest/tools'
-    filename = 'cli_test_01'
+    unitest_dir = "/tmp/magic_pdf/unittest/tools"
+    filename = "cli_test_01"
     os.makedirs(unitest_dir, exist_ok=True)
-    temp_output_dir = tempfile.mkdtemp(dir='/tmp/magic_pdf/unittest/tools')
-    os.makedirs(temp_output_dir, exist_ok=True)
+    temp_output_dir = tempfile.mkdtemp(dir="/tmp/magic_pdf/unittest/tools")
 
     def mock_read_s3_path(s3path):
-        with open(s3path, 'rb') as f:
+        with open(s3path, "rb") as f:
             return f.read()
 
-    cli_dev.read_s3_path = mock_read_s3_path  # mock
+    cli_dev.read_s3_path = mock_read_s3_path # mock
 
     # run
     runner = CliRunner()
     result = runner.invoke(
         cli_dev.cli,
         [
-            'jsonl',
-            '-j',
-            'tests/test_tools/assets/cli_dev/cli_test_01.jsonl',
-            '-o',
+            "jsonl",
+            "-j",
+            "tests/test_tools/assets/cli_dev/cli_test_01.jsonl",
+            "-o",
             temp_output_dir,
         ],
     )
@@ -93,31 +90,31 @@ def test_cli_jsonl():
     # check
     assert result.exit_code == 0
 
-    base_output_dir = os.path.join(temp_output_dir, 'cli_test_01/auto')
+    base_output_dir = os.path.join(temp_output_dir, "cli_test_01/auto")
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_content_list.json'))
+    r = os.stat(os.path.join(base_output_dir, "content_list.json"))
     assert r.st_size > 5000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}.md'))
+    r = os.stat(os.path.join(base_output_dir, f"{filename}.md"))
     assert r.st_size > 7000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_middle.json'))
+    r = os.stat(os.path.join(base_output_dir, "middle.json"))
     assert r.st_size > 200000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_model.json'))
+    r = os.stat(os.path.join(base_output_dir, "model.json"))
     assert r.st_size > 15000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_origin.pdf'))
+    r = os.stat(os.path.join(base_output_dir, "origin.pdf"))
     assert r.st_size > 500000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_layout.pdf'))
+    r = os.stat(os.path.join(base_output_dir, "layout.pdf"))
     assert r.st_size > 500000
 
-    r = os.stat(os.path.join(base_output_dir, f'{filename}_spans.pdf'))
+    r = os.stat(os.path.join(base_output_dir, "spans.pdf"))
     assert r.st_size > 500000
 
-    assert os.path.exists(os.path.join(base_output_dir, 'images')) is True
-    assert os.path.isdir(os.path.join(base_output_dir, 'images')) is True
+    assert os.path.exists(os.path.join(base_output_dir, "images")) is True
+    assert os.path.isdir(os.path.join(base_output_dir, "images")) is True
 
     # teardown
     shutil.rmtree(temp_output_dir)
