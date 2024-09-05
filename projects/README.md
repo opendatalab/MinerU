f07c267355d89f58919dd7f79591e6e4940ed517
linfeng <56671143+LollipopsAndWine@users.noreply.github.com>
2024-09-05 15:16:20 +0800

    feat: mineru_web (#555)

--- projects/README.md
+++ projects/README.md
@@ -3,3 +3,5 @@
 ## 项目列表
 
 - [llama_index_rag](./llama_index_rag/README.md): 基于 llama_index 构建轻量级 RAG 系统
+
+- [web_api](./web_api/README.md): PDF解析的restful api服务
---
+++ projects/web_api/web_api/common/web_hook.py
@@ -0,0 +1,9 @@
+
+def before_request():
+    return None
+
+
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    return response
