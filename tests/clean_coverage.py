370e7ccbe65b974141456f27a2f90da31fc1c6c2
quyuan <quyuan@pjlab.org>
2024-09-14 19:45:44 +0800

    feat: add table case

--- tests/clean_coverage.py
+++ tests/clean_coverage.py
@@ -21,5 +21,5 @@ def delete_file(path):
             print(f"Error deleting directory '{path}': {e}")
 
 if __name__ == "__main__":
-    delete_file("htmlcov")
-    delete_file(".coverage")
+    delete_file("htmlcov/")
+    #delete_file(".coverage")
