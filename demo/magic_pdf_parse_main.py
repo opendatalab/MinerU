80082d3d804bb5bd91b46bc08800ab20222b9733
samqin123 <103937568+samqin123@users.noreply.github.com>
2024-09-30 09:24:55 +0800

    Update magic_pdf_parse_main.py

--- demo/magic_pdf_parse_main.py
+++ demo/magic_pdf_parse_main.py
@@ -1,7 +1,9 @@
+## 以下代码是可以对目录中pdf文件进行循环处理，方便使用。修改前，由于文件打开后没有关闭，造成内存报错，感谢i12345的帮助，修改了代码，提交PR，请作者完善，造福大家。
+
 import os
 import json
 import copy
-
+import glob
 from loguru import logger
 
 from magic_pdf.pipe.UNIPipe import UNIPipe
@@ -11,8 +13,6 @@ from magic_pdf.rw.DiskReaderWriter import DiskReaderWriter
 import magic_pdf.model as model_config
 
 model_config.__use_inside_model__ = True
-
-# todo: 设备类型选择 （？）
 
 def json_md_dump(
         pipe,
@@ -46,23 +46,13 @@ def json_md_dump(
         path=f"{pdf_name}.md"
     )
 
-
 def pdf_parse_main(
         pdf_path: str,
-        parse_method: str = 'auto',
+        parse_method: str = 'ocr',
         model_json_path: str = None,
         is_json_md_dump: bool = True,
         output_dir: str = None
 ):
-    """
-    执行从 pdf 转换到 json、md 的过程，输出 md 和 json 文件到 pdf 文件所在的目录
-
-    :param pdf_path: .pdf 文件的路径，可以是相对路径，也可以是绝对路径
-    :param parse_method: 解析方法， 共 auto、ocr、txt 三种，默认 auto，如果效果不好，可以尝试 ocr
-    :param model_json_path: 已经存在的模型数据文件，如果为空则使用内置模型，pdf 和 model_json 务必对应
-    :param is_json_md_dump: 是否将解析后的数据写入到 .json 和 .md 文件中，默认 True，会将不同阶段的数据写入到不同的 .json 文件中（共3个.json文件），md内容会保存到 .md 文件中
-    :param output_dir: 输出结果的目录地址，会生成一个以 pdf 文件名命名的文件夹并保存所有结果
-    """
     try:
         pdf_name = os.path.basename(pdf_path).split(".")[0]
         pdf_path_parent = os.path.dirname(pdf_path)
@@ -77,21 +67,19 @@ def pdf_parse_main(
         # 获取图片的父路径，为的是以相对路径保存到 .md 和 conent_list.json 文件中
         image_path_parent = os.path.basename(output_image_path)
 
-        pdf_bytes = open(pdf_path, "rb").read()  # 读取 pdf 文件的二进制数据
+        # 使用 with open 自动处理文件的打开和关闭
+        with open(pdf_path, "rb") as pdf_file:
+            pdf_bytes = pdf_file.read()
 
         if model_json_path:
-            # 读取已经被模型解析后的pdf文件的 json 原始数据，list 类型
-            model_json = json.loads(open(model_json_path, "r", encoding="utf-8").read())
+            with open(model_json_path, "r", encoding="utf-8") as model_file:
+                model_json = json.load(model_file)
         else:
             model_json = []
 
         # 执行解析步骤
-        # image_writer = DiskReaderWriter(output_image_path)
         image_writer, md_writer = DiskReaderWriter(output_image_path), DiskReaderWriter(output_path)
 
-        # 选择解析方式
-        # jso_useful_key = {"_pdf_type": "", "model_list": model_json}
-        # pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer)
         if parse_method == "auto":
             jso_useful_key = {"_pdf_type": "", "model_list": model_json}
             pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer)
@@ -103,34 +91,36 @@ def pdf_parse_main(
             logger.error("unknown parse method, only auto, ocr, txt allowed")
             exit(1)
 
-        # 执行分类
         pipe.pipe_classify()
 
-        # 如果没有传入模型数据，则使用内置模型解析
         if not model_json:
             if model_config.__use_inside_model__:
-                pipe.pipe_analyze()  # 解析
+                pipe.pipe_analyze()
             else:
                 logger.error("need model list input")
                 exit(1)
 
-        # 执行解析
         pipe.pipe_parse()
 
-        # 保存 text 和 md 格式的结果
         content_list = pipe.pipe_mk_uni_format(image_path_parent, drop_mode="none")
         md_content = pipe.pipe_mk_markdown(image_path_parent, drop_mode="none")
-
 
         if is_json_md_dump:
             json_md_dump(pipe, md_writer, pdf_name, content_list, md_content)
 
-
     except Exception as e:
         logger.exception(e)
 
-
-# 测试
 if __name__ == '__main__':
-    pdf_path = r"C:\Users\XYTK2\Desktop\2024-2016-gb-cd-300.pdf"
-    pdf_parse_main(pdf_path)
+    pdf_directory = r"E:/MinerU/pdf_to_go"
+    pdf_files = glob.glob(os.path.join(pdf_directory, "*.pdf"))
+    output_dir = r"E:/MinerU/pdfoutput"
+
+    # 打印PDF文件列表
+    print("本次处理的PDF 文件列表:")
+    for pdf_path in pdf_files:
+        print(pdf_path)
+
+    # 循环处理每个PDF文件
+    for pdf_path in pdf_files:
+        pdf_parse_main(pdf_path, output_dir=output_dir)
