011a1b973ba487d2caf28da982ae6eff2507aa1c
myhloli <moe@myhloli.com>
2024-10-17 14:42:09 +0800

    refactor(ocr):Increase the dilation factor in OCR to address the issue of word concatenation.
    - Remove unused functions such as split_long_words, ocr_mk_mm_markdown_with_para, etc.
    - Simplify ocr_mk_markdown_with_para_core_v2 by removing unnecessary language detection and word splitting logic- Remove wordninja dependency from requirements
    - Update ocr_model_init to include additional parameters for OCR model configuration

--- magic_pdf/model/pdf_extract_kit.py
+++ magic_pdf/model/pdf_extract_kit.py
@@ -77,11 +77,11 @@ def layout_model_init(weight, config_file, device):
     return model
 
 
-def ocr_model_init(show_log: bool = False, det_db_box_thresh=0.3, lang=None):
+def ocr_model_init(show_log: bool = False, det_db_box_thresh=0.3, lang=None, use_dilation=True, det_db_unclip_ratio=2.4):
     if lang is not None:
-        model = ModifiedPaddleOCR(show_log=show_log, det_db_box_thresh=det_db_box_thresh, lang=lang)
+        model = ModifiedPaddleOCR(show_log=show_log, det_db_box_thresh=det_db_box_thresh, lang=lang, use_dilation=use_dilation, det_db_unclip_ratio=det_db_unclip_ratio)
     else:
-        model = ModifiedPaddleOCR(show_log=show_log, det_db_box_thresh=det_db_box_thresh)
+        model = ModifiedPaddleOCR(show_log=show_log, det_db_box_thresh=det_db_box_thresh, use_dilation=use_dilation, det_db_unclip_ratio=det_db_unclip_ratio)
     return model
 
 
