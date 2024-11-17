59ad273784c865f8bb6bbd4bfedbd6a24fdcfc73
quyuan <quyuan@pjlab.org>
2024-11-17 17:19:48 +0800

    update test case

--- tests/test_cli/conftest.py
+++ tests/test_cli/conftest.py
@@ -8,10 +8,3 @@ def clear_gpu_memory():
     torch.cuda.empty_cache()
     print("GPU memory cleared.")
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_teardown(item, nextitem):
-    '''
-    clear GPU memory after each test
-    '''
-    yield
-    clear_gpu_memory()
