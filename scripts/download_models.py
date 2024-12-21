66ad479953063a6f9e11de2d9717fecab9970228
chenbiao <chenbiao@globalpand.cn>
2024-12-21 18:29:16 +0800

    up

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -35,9 +35,6 @@ if __name__ == '__main__':
         "models/Layout/LayoutLMv3/*",
         "models/Layout/YOLO/*",
         "models/MFD/YOLO/*",
-        "models/MFR/unimernet_small/*",
-        "models/TabRec/TableMaster/*",
-        "models/TabRec/StructEqTable/*",
     ]
     model_dir = snapshot_download('opendatalab/PDF-Extract-Kit-1.0', allow_patterns=mineru_patterns)
     layoutreader_model_dir = snapshot_download('ppaanngggg/layoutreader')
