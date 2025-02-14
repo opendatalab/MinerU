32ec4bd0dc3219970168d7697b3140b880d68e55
lztiancn <abcstorms@163.com>
2025-02-14 23:12:51 +0800

    feat: 发送回调使用异步

--- services/fastapi/app/http_util.py
+++ services/fastapi/app/http_util.py
@@ -1,7 +1,18 @@
 import requests
-
-def post_result_callback(url, cbkey, md5_value, content_list = "", md_content = ""):
+import asyncio
+from loguru import logger
+def post_result_callback(url, cbkey, md5_value, content_list, md_content):
     if (url is None):
         return
-    json_result = {"cbkey": cbkey, "md5": md5_value, "content_list": content_list, "md_content": md_content}
-    requests.post(url, data=json_result)
+    asyncio.run(commit_callback_task(url, cbkey, md5_value, content_list, md_content))
+
+async def commit_callback_task(url, cbkey, md5_value, content_list, md_content):
+    asyncio.create_task(post_callback(url, cbkey, md5_value, content_list, md_content))
+
+async def post_callback(url, cbkey, md5_value, content_list, md_content):
+    try:
+        json_result = {"cbkey": cbkey, "md5": md5_value, "content_list": content_list, "md_content": md_content}
+        requests.post(url, data=json_result)
+        logger.info("post_callback success, url:{}", url)
+    except Exception as e:
+        logger.exception(e)
