1b611a2e5567459f17fbfd29ff04c2095d97a58e
myhloli <moe@myhloli.com>
2025-04-12 16:38:33 +0800

    build(setup): update package versions and constraints
    - Update matplotlib version range to >=3.10, <4
    - Add version上限 for ultralytics: <9
    - Remove redundant version ranges for full_old_linux

--- setup.py
+++ setup.py
@@ -40,20 +40,19 @@ if __name__ == '__main__':
                     "paddlepaddle==2.6.1;platform_system=='Windows' or platform_system=='Darwin'",
             ],
             "full": [
-                     "matplotlib<=3.9.0;platform_system=='Windows'",  # 3.9.1及之后不提供windows的预编译包，避免一些没有编译环境的windows设备安装失败
-                     "matplotlib>=3.10;platform_system=='Linux' or platform_system=='Darwin'",  # linux 和 macos 不应限制matplotlib的最高版本，以避免无法更新导致的一些bug
-                     "ultralytics>=8.3.48",  # yolov8,公式检测
+                     "matplotlib>=3.10,<4",
+                     "ultralytics>=8.3.48,<9",  # yolov8,公式检测
                      "doclayout_yolo==0.0.2b1",  # doclayout_yolo
                      "dill>=0.3.9,<1",  # doclayout_yolo
                      "rapid_table>=1.0.5,<2.0.0",  # rapid_table
                      "PyYAML>=6.0.2,<7",  # yaml
-                     "ftfy>=6.3.1,<7", # unimernet_hf
+                     "ftfy>=6.3.1,<7",  # unimernet_hf
                      "openai>=1.70.0,<2",  # openai SDK
                      "shapely>=2.0.7,<3",  # imgaug-paddleocr2pytorch
                      "pyclipper>=1.3.0,<2",  # paddleocr2pytorch
                      "omegaconf>=2.3.0,<3",  # paddleocr2pytorch
             ],
-            "full_old_linux":[
+            "full_old_linux": [
                     "matplotlib>=3.10,<=3.10.1",
                     "ultralytics>=8.3.48,<=8.3.104",  # yolov8,公式检测
                     "doclayout_yolo==0.0.2b1",  # doclayout_yolo
