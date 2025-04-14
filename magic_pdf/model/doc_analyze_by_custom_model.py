67b31a78d00e9ddf5bb55ba661820b6dd5d49128
myhloli <moe@myhloli.com>
2025-04-14 17:24:58 +0800

    fix(magic_pdf): correct range for images in document analysis
    - Update the range used to generate images_with_extra_info to match the number of images
    - This fixes a potential IndexError when the number of images differs from the dataset length

--- magic_pdf/model/doc_analyze_by_custom_model.py
+++ magic_pdf/model/doc_analyze_by_custom_model.py
@@ -147,7 +147,7 @@ def doc_analyze(
             images.append(img_dict['img'])
             page_wh_list.append((img_dict['width'], img_dict['height']))
 
-    images_with_extra_info = [(images[index], ocr, dataset._lang) for index in range(len(dataset))]
+    images_with_extra_info = [(images[index], ocr, dataset._lang) for index in range(len(images))]
 
     if len(images) >= MIN_BATCH_INFERENCE_SIZE:
         batch_size = MIN_BATCH_INFERENCE_SIZE
