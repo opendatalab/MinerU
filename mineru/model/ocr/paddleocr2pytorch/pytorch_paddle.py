8f0cc14872b3392b9952173188021c174feac29c
myhloli <moe@myhloli.com>
2025-06-11 15:39:01 +0800

    refactor: update OCR model configurations to use v5 and enhance language handling

--- mineru/model/ocr/paddleocr2pytorch/pytorch_paddle.py
+++ mineru/model/ocr/paddleocr2pytorch/pytorch_paddle.py
@@ -57,7 +57,7 @@ class PytorchPaddleOCR(TextSystem):
         self.lang = kwargs.get('lang', 'ch')
 
         device = get_device()
-        if device == 'cpu' and self.lang in ['ch', 'ch_server']:
+        if device == 'cpu' and self.lang in ['ch', 'ch_server', 'japan', 'chinese_cht']:
             logger.warning("The current device in use is CPU. To ensure the speed of parsing, the language is automatically switched to ch_lite.")
             self.lang = 'ch_lite'
 
