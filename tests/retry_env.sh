b457df4aea5e909397fddd0c93678bda5de189a1
zhanluxianshen <zhanluxianshen@163.com>
2025-07-08 05:50:11 +0800

    fix tests/retry_env.sh log info.
    Signed-off-by: zhanluxianshen <zhanluxianshen@163.com>

--- tests/retry_env.sh
+++ tests/retry_env.sh
@@ -14,7 +14,7 @@ while true; do
     python download_models.py
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
-        echo "test.sh 成功执行！"
+        echo "retry_env.sh 成功执行！"
         break
     else
         let retry_count+=1
@@ -22,7 +22,7 @@ while true; do
             echo "达到最大重试次数 ($max_retries)，放弃重试。"
             exit 1
         fi
-        echo "test.sh 执行失败 (退出码: $exit_code)。尝试第 $retry_count 次重试..."
+        echo "retry_env.sh 执行失败 (退出码: $exit_code)。尝试第 $retry_count 次重试..."
         sleep 5
     fi
 done
