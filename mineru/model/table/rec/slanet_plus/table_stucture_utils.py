9d10bb13f5283553a1be4dacc4cd41d41aa7ea4f
Sidney233 <910983568@qq.com>
2025-08-20 11:15:41 +0800

    fix: remove unitable

--- mineru/model/table/rec/slanet_plus/table_stucture_utils.py
+++ mineru/model/table/rec/slanet_plus/table_stucture_utils.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -*- encoding: utf-8 -*-
-# @Author: SWHL
-# @Contact: liekkaskono@163.com
 import os
 import platform
 import traceback
