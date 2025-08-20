17a7758feecc5ba234c52f86504a546b9cec53fe
Sidney233 <910983568@qq.com>
2025-08-20 11:17:37 +0800

    fix: remove unitable

--- mineru/model/table/rec/slanet_plus/matcher.py
+++ mineru/model/table/rec/slanet_plus/matcher.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -*- encoding: utf-8 -*-
 import numpy as np
 
 from .matcher_utils import compute_iou, distance
