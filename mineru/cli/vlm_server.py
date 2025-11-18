506179f0c82aaf3905afe4e5c8c2d0d7826a1db0
myhloli <moe@myhloli.com>
2025-11-18 15:28:19 +0800

    feat: add openai-server command for flexible inference engine selection in vlm_server

--- mineru/cli/vlm_server.py
+++ mineru/cli/vlm_server.py
@@ -1,3 +1,7 @@
+import click
+import sys
+
+from loguru import logger
 
 
 def vllm_server():
@@ -8,3 +12,32 @@ def vllm_server():
 def lmdeploy_server():
     from mineru.model.vlm.lmdeploy_server import main
     main()
+
+
+@click.command(context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
+@click.option(
+    '-e',
+    '--engine',
+    'inference_engine',
+    type=click.Choice(['auto', 'vllm', 'lmdeploy']),
+    default='auto',
+    help='Select the inference engine used to accelerate VLM inference, default is "auto".',
+)
+@click.pass_context
+def openai_server(ctx, inference_engine):
+    sys.argv = [sys.argv[0]] + ctx.args
+    if inference_engine == 'auto':
+        try:
+            import vllm
+            inference_engine = 'vllm'
+            logger.info("Using vLLM as the inference engine for VLM server.")
+        except ImportError:
+            inference_engine = 'lmdeploy'
+            logger.info("vLLM not found, falling back to LMDeploy as the inference engine for VLM server.")
+    if inference_engine == 'vllm':
+        vllm_server()
+    elif inference_engine == 'lmdeploy':
+        lmdeploy_server()
+
+if __name__ == "__main__":
+    openai_server()
