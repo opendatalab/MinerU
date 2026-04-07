fede2923886016acf0ebd04fd7f467158a2c1087
myhloli <moe@myhloli.com>
2026-04-07 20:49:26 +0800

    feat: adjust contrast threshold for OCR processing in span_pre_proc.py

--- mineru/utils/span_pre_proc.py
+++ mineru/utils/span_pre_proc.py
@@ -127,7 +127,7 @@ def _prepare_post_ocr_spans(need_ocr_spans, spans, pil_img, scale):
         # 对span的bbox截图再ocr
         span_pil_img = get_crop_img(span['bbox'], pil_img, scale)
         span_img = cv2.cvtColor(np.array(span_pil_img), cv2.COLOR_RGB2BGR)
-        # 计算span的对比度，低于0.20的span不进行ocr
+        # 计算span的对比度，低于0.17的span不进行ocr
         if calculate_contrast(span_img, img_mode='bgr') <= 0.17:
             if span in spans:
                 spans.remove(span)
